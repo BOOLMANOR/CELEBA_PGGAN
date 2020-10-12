ea725570bf319dbc928b76e32cd7ce77ffbf1267
Au <lrigi@outlook.com>
2020-10-12 15:41:51 +0800

    修改路径

--- utils.py
+++ utils.py
@@ -66,7 +66,7 @@ def CV2_IMSHOW_NHWC_RAMDOM(NHWC, scale, N, rows, cols,title='img', delay = 0):
     CV2_IMSHOW_NHWC(rands, scale, rows, cols, title, delay)
 
 # 人脸检测
-face_cascade = cv2.CascadeClassifier(r'F:\ww\CELEBA_PGGAN\haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier(r'haarcascade_frontalface_default.xml')
 def CV2_CROP_FACE(img):
     bboxses = face_cascade.detectMultiScale(img, 1.3, 5)
     face = None
